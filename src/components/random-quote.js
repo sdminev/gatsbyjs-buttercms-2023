e43f1cf6bfb440ba860af3509aa0cdaf57a0c126
Stefan Minev <68925860+sdminev@users.noreply.github.com>
2023-05-07 22:49:49 +0300

    no message

--- src/components/random-quote.js
+++ src/components/random-quote.js
@@ -1,34 +1,40 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const RandomQuote = () => {
-  const [quote, setQuote] = useState(null)
+  const [quote, setQuote] = useState(null);
 
   useEffect(() => {
     axios
       .get(
-        `https://api.buttercms.com/v2/content/?auth_token=${process.env.GATSBY_BUTTER_CMS_API_KEY}&keys=quotes`
+        `https://api.buttercms.com/v2/content/?auth_token=9a50fb6252ff0804582816d7ac5f83b89a1364eb&keys=quotes`
       )
-      .then(response => {
-        const quotes = response.data.data.quotes
-        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-        setQuote(randomQuote)
+      .then((response) => {
+        const quotes = response.data.data.quotes;
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        setQuote(randomQuote);
       })
-      .catch(error => {
-        console.error(error)
-      })
-  }, [])
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
       {quote && (
-        <div>
-          <h2>{quote.author}</h2>
-          <p>{quote.quote_text}</p>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6 col-md-10">
+              <div className="section-title text-center">
+                <h2>{quote.quote_text}</h2>
+                <p>{quote.author}</p>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default RandomQuote
+export default RandomQuote;

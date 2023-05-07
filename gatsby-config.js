de2690cfda9a3c2d840fee00dd562a455ba0fbca
Stefan Minev <68925860+sdminev@users.noreply.github.com>
2023-05-07 13:55:39 +0300

    update gatsby-config to include projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         // Collections you want the app to have access to should have their
         // slugs explicitly included in the keys array, as seen below.
         contentFields: {
-          keys: [`navigation_menu`],
+          keys: [`navigation_menu`, `projects`],
           // Optional. Set to 1 to enable test mode for viewing draft content.
           test: previewMode,
         },

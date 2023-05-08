3483b4892deefa18278207762cbb7108c5bd4103
Stefan Minev <68925860+sdminev@users.noreply.github.com>
2023-05-08 20:16:38 +0300

    no message

--- src/templates/index.js
+++ src/templates/index.js
@@ -20,11 +20,7 @@ console.log(projects)
 
       <HeroSection fields={bodyElements[0].fields} />
       <div className="quotes"> <RandomQuote /></div>
-      {projects.map(project => {
-        return (
-          <div key={project.name}>{ project.name }</div>
-        )
-      })}
+
       {bodyElements.map((bodyElement, i) => {
         switch (bodyElement.type) {
           case "two_column_with_image":
